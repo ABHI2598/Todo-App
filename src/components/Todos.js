d1629e7ddd5987ea6a8430da889ace0140e7d4f7
Abhinav Pandey <pandeyabhinav56@gmail.com>
2023-09-05 21:47:34 +0530

    added input

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -7,7 +7,7 @@ import { addTodo } from "../utils/TodoSlice";
 
 
 const Todos = () => {
-    const [text , setText] = useState();
+    const [text , setText] = useState("");
     const storedTodos = useSelector(store => store.todoSlice.todos);
      
     const dispatch = useDispatch();
@@ -15,10 +15,26 @@ const Todos = () => {
         dispatch(addTodo(text));
         setText("");
     }
+
+    const debounce = (call,d) => {
+        let timer;
+        return function()
+        {
+           let context = this,
+           args = arguments
+           if(timer) clearTimeout(timer);
+           timer = setTimeout(() =>{
+            call.apply(context,args);
+           },d);
+        }
+    }
+    const handleChange = (e) =>{
+        setText(e.target.value);
+    }
     return(
         <div>
         <div className="absolute top-[24%] left-[30%] right-[35%]">
-            <input type="text" className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" placeholder="Create a new todo..." value={text} onChange={(e) => setText(e.target.value)}/>
+            <input type="text" className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" placeholder="Create a new todo..." value={text} onChange={handleChange}/>
         </div>
         <div className="absolute top-[24.3%] left-[59.6%] right-[31%]">
                 <button className="py-2 px-2 w-20 border bg-blue-500 hover:bg-blue-400 focus:ring-1 focus:outline-none focus:ring-blue-300 text-white font-bold border-grey-500 rounded-full  " onClick={()=> handleAdd()}>Add</button>
